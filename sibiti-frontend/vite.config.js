63b3fa1590141a8d55423d7106d7ab9e419331e2
psmfajru1107 <fajrul.ichsankamil@ai.astra.co.id>
2024-04-18 16:56:24 +0700

    update config

--- sibiti-frontend/vite.config.js
+++ sibiti-frontend/vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    cors: true,
+  },
+  build: {
+    target: 'esnext',
+  },
+  plugins: [react(),
+    // Plugin untuk mengatur default Content-Type menjadi application/x-www-form-urlencoded
+    {
+      name: 'set-content-type',
+      transformIndexHtml(html) {
+        return html.replace('<head>', '<head><meta http-equiv="Content-Type" content="application/x-www-form-urlencoded">');
+      },
+    },
+  ],
+});
